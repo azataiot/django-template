cf06420b4c53aaf269d9d958a8c735b2a1aeb45f
Azat <8280770+azataiot@users.noreply.github.com>
2023-09-01 13:46:25 +0300

    It seems like the NEW_VERSION variable is not being set correctly for feature branches, which is causing the poetry-check and poetry-lock hooks to fail.

--- scripts/create-branch.sh
+++ scripts/create-branch.sh
@@ -3,7 +3,6 @@
 # Get the current version from pyproject.toml
 CURRENT_VERSION=$(python -c "import tomllib; print(tomllib.loads(open('pyproject.toml').read())['tool']['poetry']['version'])")
 
-
 # Select branch type
 PS3="Select branch type: "
 options=("release" "hotfix" "feature" "temp")
@@ -35,16 +34,18 @@ if [ "$BRANCH_TYPE" == "release" ]; then
 fi
 
 # Increment the version number according to PEP 440
-if [ "$BRANCH_TYPE" == "release" ]; then
+if [ "$BRANCH_TYPE" == "release" ] || [ "$BRANCH_TYPE" == "hotfix" ]; then
     cd scripts || exit
     NEW_VERSION=$(python -c "import increment_version; print(increment_version.increment_version('$CURRENT_VERSION', '$RELEASE_TYPE'))")
     cd .. || exit
     BRANCH_NAME="$BRANCH_TYPE-$NEW_VERSION"
-elif [ "$BRANCH_TYPE" == "hotfix" ]; then
-    cd scripts || exit
-    NEW_VERSION=$(python -c "import increment_version; print(increment_version.increment_version('$CURRENT_VERSION', 'final'))")
-    cd .. || exit
-    BRANCH_NAME="$BRANCH_TYPE-$NEW_VERSION"
+
+    # Update the version in pyproject.toml
+    sed -i.bak "s/version = \"$CURRENT_VERSION\"/version = \"$NEW_VERSION\"/" pyproject.toml && rm pyproject.toml.bak
+
+    # Commit the changes
+    git add pyproject.toml
+    git commit -m "Bumped version number to $NEW_VERSION"
 elif [ "$BRANCH_TYPE" == "feature" ]; then
     read -p "Enter feature name: " FEATURE_NAME
     BRANCH_NAME="$BRANCH_TYPE-$(echo $FEATURE_NAME | sed 's/ /-/g')"
@@ -52,14 +53,7 @@ else
     BRANCH_NAME="$BRANCH_TYPE-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)"
 fi
 
-# Update the version in pyproject.toml
-sed -i.bak "s/version = \"$CURRENT_VERSION\"/version = \"$NEW_VERSION\"/" pyproject.toml && rm pyproject.toml.bak
-
 # Create and switch to the new branch
 git checkout -b "$BRANCH_NAME"
-
-# Commit the changes
-git add pyproject.toml
-git commit -m "Bumped version number to $NEW_VERSION"
 
 echo "Created new branch $BRANCH_NAME with version number $NEW_VERSION"
