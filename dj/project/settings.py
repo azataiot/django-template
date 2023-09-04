8e61b31d7975d01a024b1d936e10225bdb7151d1
Azat <8280770+azataiot@users.noreply.github.com>
2023-09-04 17:02:27 +0300

    dj-database-url just did not work out

--- dj/project/settings.py
+++ dj/project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-import dj_database_url
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -103,16 +102,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# https://github.com/jazzband/dj-database-url
-DATABASE_URL = env("DATABASE_URL", default="sqlite:///db.sqlite3")
 DATABASES = {
-    "default": dj_database_url.config(
-        default="postgres://postgres:postgres@db/django",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
