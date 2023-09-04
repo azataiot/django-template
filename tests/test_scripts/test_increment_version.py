10317f148d97eeea772577cd4e69c3a0f83495e3
Azat <8280770+azataiot@users.noreply.github.com>
2023-09-04 13:20:00 +0300

    changed the test code according to PEP440

--- tests/test_scripts/test_increment_version.py
+++ tests/test_scripts/test_increment_version.py
@@ -3,20 +3,40 @@ import pytest
 from scripts.increment_version import increment_version
 
 
-@pytest.mark.parametrize(
-    "version, release_type, expected",
-    [
-        ("0.0.2a", "alpha", "0.0.3a"),
-        ("0.0.2a", "beta", "0.0.1b"),
-        ("0.0.2a", "candidate", "0.0.1rc"),
-        ("0.0.2a", "breaking", "1.0.0"),
-        ("0.0.2a", "final", "0.0.3"),
-        ("0.0.2", "alpha", "0.0.1a"),
-        ("0.0.2b", "beta", "0.0.3b"),
-        ("0.0.2rc", "candidate", "0.0.3rc"),
-        ("1.0.0", "breaking", "2.0.0"),
-        ("1.0.0", "final", "1.0.1"),
-    ],
-)
-def test_increment_version(version, release_type, expected):
-    assert increment_version(version, release_type) == expected
+def test_increment_alpha():
+    assert increment_version("1.2.3", "a") == "1.2.1a"
+    assert increment_version("1.2.3a", "a") == "1.2.4a"
+
+
+def test_increment_beta():
+    assert increment_version("1.2.3", "b") == "1.2.1b"
+    assert increment_version("1.2.3b", "b") == "1.2.4b"
+
+
+def test_increment_rc():
+    assert increment_version("1.2.3", "rc") == "1.2.1rc"
+    assert increment_version("1.2.3rc", "rc") == "1.2.4rc"
+
+
+def test_increment_post():
+    assert increment_version("1.2.3", "post") == "1.2.1post"
+    assert increment_version("1.2.3post", "post") == "1.2.4post"
+
+
+def test_increment_dev():
+    assert increment_version("1.2.3", "dev") == "1.2.1dev"
+    assert increment_version("1.2.3dev", "dev") == "1.2.4dev"
+
+
+def test_increment_final():
+    assert increment_version("1.2.3", "final") == "1.2.4"
+    assert increment_version("1.2.3a", "final") == "1.2.4"
+    assert increment_version("1.2.3b", "final") == "1.2.4"
+    assert increment_version("1.2.3rc", "final") == "1.2.4"
+    assert increment_version("1.2.3post", "final") == "1.2.4"
+    assert increment_version("1.2.3dev", "final") == "1.2.4"
+
+
+def test_invalid_release_type():
+    with pytest.raises(ValueError):
+        increment_version("1.2.3", "invalid")
